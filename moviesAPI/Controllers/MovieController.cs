e960d7eb881085bae64d88d1f0ce1f5c63b53416
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-09-02 22:28:19 -0300

    Add second class Class about how to post a new movie and how to use data annotations to validate input data

---
+++ moviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using moviesAPI.Models;
+
+namespace moviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> _movies = new();
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            _movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+    }
+}
